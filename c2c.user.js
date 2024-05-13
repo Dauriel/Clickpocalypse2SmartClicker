7734654611a439810d99ec6f101f59b176a9a02d
Pablo <pabloizaya@gmail.com>
2024-05-13 15:42:18 +0200

    Update c2c.user.js
    - Added custom strategy

--- c2c.user.js
+++ c2c.user.js
@@ -4,7 +4,7 @@
 // @description Clicker Bot for Clickpocalypse2
 // @include     http://minmaxia.com/c2/
 // @include     https://minmaxia.com/c2/
-// @version     1.0.8
+// @version     1.0.9
 // @grant       none
 // @require https://code.jquery.com/jquery-3.1.0.slim.min.js
 // ==/UserScript==
@@ -29,26 +29,50 @@ const CHAR_CLASS_LIST = [
   "King",
   "Spider Lord"
 ];
-// skill points upgrade strategy
+
+
+// 1 for default strategy, 2 for summoning strategy
+const strategytype = {
+  "Fighter": 1,
+  "Priest": 1,
+  "Ranger": 1,
+  "Pyro": 1,
+  "Rogue": 1,
+  "Druid": 0,
+  "Barbarian": 1,
+  "Electro": 1,
+  "Ninja": 1,
+  "Necro": 1,
+  "King": 1,
+  "Lord": 1,
+}
+
+// default points upgrade strategy
 const skillStrategy = {
-  "Fighter" : [0, 1, 2, 3],
-  "Priest" : [2, 1, 4, 0],
-  "Ranger" : [0, 1, 2, 3],
-  "Pyro" : [3, 2, 1, 0],
-  "Rogue" : [3, 2, 1, 0],
-  "Druid" : [0, 3, 1, 2],
-  "Barbarian" : [0, 1, 3, 2],
-  "Electro" : [3, 2, 1, 0],
-  "Ninja" : [0, 3, 1, 2],
-  "Necro" : [1, 2, 0, 3],
-  "King" : [0, 1, 2, 3],
-  "Lord" : [0, 3, 1, 2],
+  "Fighter": [0, 1, 2, 3],
+  "Priest": [2, 1, 4, 0],
+  "Ranger": [0, 1, 2, 3],
+  "Pyro": [3, 2, 1, 0],
+  "Rogue": [3, 2, 1, 0],
+  "Druid": [0, 3, 1, 2],
+  "Barbarian": [0, 1, 3, 2],
+  "Electro": [3, 2, 1, 0],
+  "Ninja": [0, 3, 1, 2],
+  "Necro": [1, 2, 0, 3],
+  "King": [0, 1, 2, 3],
+  "Lord": [0, 3, 1, 2],
 };
+
+// rudimentary summoning strategy. [col, row]
+const summonStrategy = {
+  "Druid": [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [2, 8], [1, 7], [1, 8], [3, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8]],
+}
+
 // interval time
 const INTERVAL = 1000;
 
 let configs = {
-	autoPoint: false,
+  autoPoint: false,
   autoSkill: true,
   autoScroll: true,
   autoPotion: true,
@@ -57,8 +81,8 @@ let configs = {
 };
 
 function updateConfig(name) {
-	return function() {
-  	if ([
+  return function () {
+    if ([
       "autoPoint",
       "autoSkill",
       "autoScroll",
@@ -72,7 +96,7 @@ function updateConfig(name) {
     ].includes(name)) {
       // validate scale value
       if (isNaN(this.value)) {
-      	alert("Window transform scale must be a valid number");
+        alert("Window transform scale must be a valid number");
         return;
       }
       configs.windowScale = parseFloat(this.value);
@@ -87,7 +111,7 @@ function updateConfig(name) {
 function rendMenu() {
   let container = $("#script-ui");
   container.empty();
-	let markup = `
+  let markup = `
 		 <table>
         <tr>
           <td align=right>Auto AP Point:</td>
@@ -129,56 +153,56 @@ $(document).ready(function () {
   console.log("Add script manager UI");
   $("body").append($(`<div id='script-ui' style="background: #000;position: fixed;bottom: 15px;right: 15px; padding: 10px; border: 1px solid #1e1b1b;"></div>`));
   rendMenu();
-  
-	console.log('Starting Clickpocalypse2Clicker: ' + GM_info.script.version);
-  
-	setInterval(function () {
-  	let charClasses = findClasses();
+
+  console.log('Starting Clickpocalypse2Clicker: ' + GM_info.script.version);
+
+  setInterval(function () {
+    let charClasses = findClasses();
     if (!charClasses || !charClasses.length) return; // this means the game is not started
-    
+
     // ------------------------------------ BOSS strategy ------------------------------------
-		// Determines our encounter states
-		var isBossEncounter = ($('.bossEncounterNotificationDiv').length != 0);
-		var isEncounter = ($('#encounterNotificationPanel').css('display') !== 'none');
-		//console.log("Boss: " +isBossEncounter +" Normal: " +isEncounter);
+    // Determines our encounter states
+    var isBossEncounter = ($('.bossEncounterNotificationDiv').length != 0);
+    var isEncounter = ($('#encounterNotificationPanel').css('display') !== 'none');
+    //console.log("Boss: " +isBossEncounter +" Normal: " +isEncounter);
 
-		// Determine if this is a difficult encounter... (one or more characters are stunned).
-		//todo: should cancel search once we find it to be true.
-		var isDifficultEncounter = false;
-		// slot positions.
-		var pos = ['A', 'B', 'C', 'E', 'E', 'F'];
-		$.each(pos, function (idx) {
-			var letter = pos[idx];
+    // Determine if this is a difficult encounter... (one or more characters are stunned).
+    //todo: should cancel search once we find it to be true.
+    var isDifficultEncounter = false;
+    // slot positions.
+    var pos = ['A', 'B', 'C', 'E', 'E', 'F'];
+    $.each(pos, function (idx) {
+      var letter = pos[idx];
 
-			// character positions.
-			for (var char = 0; char < 5; char++) {
+      // character positions.
+      for (var char = 0; char < 5; char++) {
 
-				var name = '#adventurerEffectIcon' + letter + char;
-				var selector = $(name);
-				//console.log("Checking: " + name + " Title: " + selector.attr('title') + " Display " + selector.css('display') + " HTML: " +selector.html());
-				if (selector.attr('title') === 'Stunned' && selector.css('display') !== 'none') {
-					isDifficultEncounter = true;
+        var name = '#adventurerEffectIcon' + letter + char;
+        var selector = $(name);
+        //console.log("Checking: " + name + " Title: " + selector.attr('title') + " Display " + selector.css('display') + " HTML: " +selector.html());
+        if (selector.attr('title') === 'Stunned' && selector.css('display') !== 'none') {
+          isDifficultEncounter = true;
 
-				}
-			}
-		});
+        }
+      }
+    });
 
-		//console.log("isDifficultEncounter: " + isDifficultEncounter);
+    //console.log("isDifficultEncounter: " + isDifficultEncounter);
 
     // --------------------------------------- Loot ---------------------------------------
-		// loot them chests... not sure which one of these is working.
-		if (configs.autoLoot) {
-    	clickSelector($('#treasureChestLootButtonPanel').find('.gameTabLootButtonPanel'));
-			clickSelector($('#treasureChestLootButtonPanel').find('.lootButton'));
+    // loot them chests... not sure which one of these is working.
+    if (configs.autoLoot) {
+      clickSelector($('#treasureChestLootButtonPanel').find('.gameTabLootButtonPanel'));
+      clickSelector($('#treasureChestLootButtonPanel').find('.lootButton'));
 
       // Cycle though all quick bar upgrades in reverse order.
       for (var i = 43; i >= 0; i--) {
         clickIt('#upgradeButtonContainer_' + i);
       }
     }
-		// --------------------------------------- AP points -----------------------------------
-		// Update AP Upgrades
-		if (configs.autoPoint) {
+    // --------------------------------------- AP points -----------------------------------
+    // Update AP Upgrades
+    if (configs.autoPoint) {
       for (var row = 0; row < 12; row++) {
         // skip 'Offline Time Bonus' upgrade.
         if (row == 3) {
@@ -192,8 +216,8 @@ $(document).ready(function () {
         }
       }
     }
-		// ----------------------------------------- potions ----------------------------------------
-		if (configs.autoPotion) {
+    // ----------------------------------------- potions ----------------------------------------
+    if (configs.autoPotion) {
       // Get information about potions are active before taking any actions
       var isPotionActive_ScrollsAutoFire = false;
       var isPotionActive_InfinteScrolls = false;
@@ -270,8 +294,8 @@ $(document).ready(function () {
               }
             }
 
-            if ( (potionName === 'Random Treasure Room' || potionName === 'Double Item Drops' || potionName === 'Double Gold Drops')  
-              && (isPotionActive_InfinteScrolls || isPotionActive_ScrollsAutoFire) ) {
+            if ((potionName === 'Random Treasure Room' || potionName === 'Double Item Drops' || potionName === 'Double Gold Drops')
+              && (isPotionActive_InfinteScrolls || isPotionActive_ScrollsAutoFire)) {
               continue;
             }
 
@@ -281,15 +305,15 @@ $(document).ready(function () {
         }
       }
     }
-		// ------------------------------------ scrolls ------------------------------------------
-		if (configs.autoScroll) {
-    	// Get info about scrolls before taking any action.
+    // ------------------------------------ scrolls ------------------------------------------
+    if (configs.autoScroll) {
+      // Get info about scrolls before taking any action.
       var totalScrolls = 0;
       for (var i = 0; i < 6; i++) {
 
         var scrollCell = $('#scrollButtonCell' + i);
         var scrollButton = scrollCell.find('.scrollButton');
-        var scrollAmount = scrollCell.find('tr').eq(1).text().replace('x', ''); ;
+        var scrollAmount = scrollCell.find('tr').eq(1).text().replace('x', '');;
 
         if (!scrollAmount.length) {
           continue;
@@ -314,7 +338,7 @@ $(document).ready(function () {
 
         var scrollCell = $('#scrollButtonCell' + i);
         var scrollButton = scrollCell.find('.scrollButton');
-        var scrollAmount = scrollCell.find('tr').eq(1).text().replace('x', ''); ;
+        var scrollAmount = scrollCell.find('tr').eq(1).text().replace('x', '');;
 
         if (!scrollAmount.length) {
           continue;
@@ -362,11 +386,12 @@ $(document).ready(function () {
       }
     }
 
-	}, INTERVAL);
-  
-  // ---------------------------- skill points interval -------------------------------
+  }, INTERVAL);
+
+  // ---------------------------- summon interval -------------------------------
+
   let clicked = null;
-  setInterval(function() {
+  setInterval(function () {
     if (!hasSkillPoint()) {
       // nothing to be upgraded
       clicked = null;
@@ -382,16 +407,34 @@ $(document).ready(function () {
       // after a certion time while you just upgrade one. At the same time, the other "clickable" skills will be clicked during
       // that certian time. a solution would be up grade only 1 skill for each class at one interval.
       loopChar: for (var charPos = 0; charPos < charClasses.length; charPos++) {
-        let strategy = skillStrategy[charClasses[charPos]];
-        if (!strategy) strategy = [0, 1, 2, 3];
-        // try through the columns following the strategy
-        loopStrategy: for (var s of strategy) {
-          loopRow: for (var row = 0; row < 9; row++) {
+
+        if (strategytype[charClasses[charPos]]) {
+          console.log("Upgrading skills for " + charClasses[charPos] + " using default strategy");
+          let strategy = skillStrategy[charClasses[charPos]];
+          loopStrategy: for (var s of strategy) {
+            loopRow: for (var row = 0; row < 9; row++) {
+              // There is an ending col on all, not sure why yet
+              let id = charPos + '_' + row + '_' + s + '_' + row;
+              if (clicked[charPos].indexOf(id) < 0) {
+                clickIt('#characterSkillsContainer' + id);
+                clicked[charPos].push(id);
+                console.log("Upgraded skill for " + charClasses[charPos] + " using default strategy at column" + s + "row" + row)
+                break loopStrategy; // done for this char
+              }
+            }
+          }
+        } else {
+          console.log("Upgrading skills for " + charClasses[charPos] + " using custom summoning strategy");
+          let strategy = summonStrategy[charClasses[charPos]];
+
+          // let's find the current index of the summon strategy
+          loopStrategy: for (var s of strategy) {
             // There is an ending col on all, not sure why yet
-            let id = charPos + '_' + row + '_' + s + '_' + row;
+            let id = charPos + '_' + s[1] + '_' + s[0] + '_' + s[1];
             if (clicked[charPos].indexOf(id) < 0) {
               clickIt('#characterSkillsContainer' + id);
               clicked[charPos].push(id);
+              console.log("Upgraded skill for " + charClasses[charPos] + " using custom summoning strategy at column" + s[0] + "row" + s[1])
               break loopStrategy; // done for this char
             }
           }
@@ -399,27 +442,28 @@ $(document).ready(function () {
       }
     }
   }, INTERVAL);
+
 });
 // -------------------------------- helpers -----------------------------------
 /*** Click by div id **/
 function clickIt(divName) {
-	var div = $(divName);
-	if (!div.length) {
-		return;
-	} // They use mouse up instead of click()
+  var div = $(divName);
+  if (!div.length) {
+    return;
+  } // They use mouse up instead of click()
 
-	div.mouseup();
+  div.mouseup();
 }
 /*** Click by Selector **/
 function clickSelector($selector) {
-	$selector.mouseup();
+  $selector.mouseup();
 }
 // find char classes
 function findClasses() {
   let charClasses = [];
   let elements = document.querySelectorAll("#gameTabMenu li a");
   if (elements && elements.length)
-    elements.forEach(function(e){
+    elements.forEach(function (e) {
       let name = e.text.split(" ")[0];
       if (CHAR_CLASS_LIST.indexOf(name) <= 0) return;
       charClasses.push(name);
@@ -431,7 +475,7 @@ function hasSkillPoint(charClasses) {
   let elements = document.querySelectorAll("#gameTabMenu li a");
   let hasPoint = false;
   if (elements && elements.length)
-    elements.forEach(function(e){
+    elements.forEach(function (e) {
       let name = e.text.split(" ")[0];
       if (CHAR_CLASS_LIST.indexOf(name) <= 0) return;
       if (e.text.split(" ").length > 1) hasPoint = true;

6be4f6d13f80140f3350ff5ad44cd837a234c790
Pablo <pabloizaya@gmail.com>
2024-05-14 12:04:58 +0200

    Update c2c.user.js
    - Renamed to customStrategy

--- c2c.user.js
+++ c2c.user.js
@@ -63,8 +63,8 @@ const skillStrategy = {
   "Lord": [0, 3, 1, 2],
 };
 
-// rudimentary summoning strategy. [col, row]
-const summonStrategy = {
+// rudimentary custom strategy. [col, row]
+const customStrategy = {
   "Druid": [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [1, 1], [1, 2], [2, 8], [0, 7], [0, 8], [3, 0], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8]],
 }
 
@@ -425,7 +425,7 @@ $(document).ready(function () {
           }
         } else {
           //console.log("Upgrading skills for " + charClasses[charPos] + " using custom summoning strategy");
-          let strategy = summonStrategy[charClasses[charPos]];
+          let strategy = customStrategy[charClasses[charPos]];
 
           // let's find the current index of the summon strategy
           loopStrategy: for (var s of strategy) {
